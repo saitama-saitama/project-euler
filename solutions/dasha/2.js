6ac9e8375047de419cf7db06131fee8038063ecc
Dasha Zholnerchik <ggdasha0601@gmail.com>
2024-06-17 02:22:47 +0300

    initial commit

---
+++ solutions/dasha/2.js
@@ -0,0 +1,17 @@
+function sumEvenFibonacciNumbers(n) {
+    let a = 1, b = 2, sum = 2; // we start with 2, as 2 is the first even Fibonacci number
+  
+    while (b <= n) {
+      let temp = b;
+      b = a + b;
+      a = temp;
+  
+      if (b % 2 === 0) {
+        sum += b;
+      }
+    }
+  
+    return sum;
+  }
+  
+  console.log(sumEvenFibonacciNumbers(4000000)); 
