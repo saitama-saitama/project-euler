28f6c52bf584d6c0a2f958915a94341d291be46a
Dasha Zholnerchik <ggdasha0601@gmail.com>
2024-06-21 05:22:14 +0300

    initial commit

--- solutions/dasha/2.js
+++ solutions/dasha/2.js
@@ -1,17 +1,18 @@
-function sumEvenFibonacciNumbers(n) {
-    let a = 1, b = 2, sum = 2; // we start with 2, as 2 is the first even Fibonacci number
+function sumEvenFibonacciNumbers() {
+  let n = prompt("Enter a number:");
+  let a = 1, b = 2, sum = 2; // we start with 2, as 2 is the first even Fibonacci number
   
-    while (b <= n) {
-      let temp = b;
-      b = a + b;
-      a = temp;
+  while (b <= n) {
+    let temp = b;
+    b = a + b;
+    a = temp;
   
-      if (b % 2 === 0) {
-        sum += b;
-      }
+    if (b % 2 === 0) {
+      sum += b;
     }
-  
-    return sum;
   }
   
-  console.log(sumEvenFibonacciNumbers(4000000)); 
+  return sum;
+}
+
+console.log(sumEvenFibonacciNumbers());
--- solutions/dasha/3.js
+++ solutions/dasha/3.js
@@ -1,20 +1,23 @@
+const number = Number(prompt('Enter a number:'));
+
 function largestPrimeFactor(number) {
-    let largestPrime = 1;
-    let currentFactor = 2;
+  let largestPrime = 1;
+  let currentFactor = 2;
   
-    while (currentFactor * currentFactor <= number) {
-      if (number % currentFactor === 0) {
-        largestPrime = currentFactor;
-        number /= currentFactor;
-      } else {
-        currentFactor += 1;
-      }
+  while (currentFactor * currentFactor <= number) {
+    if (number % currentFactor === 0) {
+      largestPrime = currentFactor;
+      number /= currentFactor;
+    } else {
+      currentFactor += 1;
     }
-  
-    if (number > 2) {
-      largestPrime = number;
-    }
-  
-    return largestPrime;
   }
-  console.log(largestPrimeFactor(13195)); 
+  
+  if (number > 2) {
+    largestPrime = number;
+  }
+  
+  return largestPrime;
+}
+
+console.log("The largest prime factor of" + number + "is" + largestPrimeFactor(number));
